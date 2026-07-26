8bb7bce1115ebc3e445b61060a7ce434f4e744d4
Michelle1718 <macedomichelle69@gmail.com>
2026-07-26 15:58:17 -0300

    primeiro commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,39 @@
 /*=============== SHOW & CLOSE MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
+/* Show Menu */
+if (navToggle) {
+   navToggle.addEventListener('click', () => {
+      navMenu.classList.add('show-menu');
+   });
+}
 
+/* Hide Menu */
+if (navClose) {
+   navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu');
+   });
+}
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () => {
+    //quando clicamos no nav__link, removemos a classe show-menu
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE HEADER STYLES ===============*/
+const scrollHeader = () =>{
+const header = document.getElementById('header')
+   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
+       this.scrollY >= 50 ? header.classList.add('scroll-header')
+                           : header.classList.remove('scroll-header')
+      }
+     window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SWIPER MENU ===============*/ 
